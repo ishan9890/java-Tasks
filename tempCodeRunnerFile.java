50bc541be9255977f6c71ec4ec1e5d02a9c3b8dc
Ishan <ishanjoshi234@gmail.com>
2025-04-26 21:13:27 +0545

    Initial

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class hello{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char variableName ='a';
+        System.out.print("Enter the age ");
+        int age=sc.nextInt();
+        System.out.print("Enter the name ");
+        String name = sc.next();
